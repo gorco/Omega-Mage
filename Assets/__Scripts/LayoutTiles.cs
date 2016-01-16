b4eebdf99fa4b17ce3aa80f94d8b7ce0329f7301
gorco <antcal01@ucm.es>
2016-01-16 17:47:01 +0100

    Added enemies and fire attack

--- Assets/__Scripts/LayoutTiles.cs
+++ Assets/__Scripts/LayoutTiles.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+
 [System.Serializable]
 public class TileTex
 {
@@ -8,6 +9,15 @@ public class TileTex
 	public string str;
 	public Texture2D tex;
 }
+
+[System.Serializable]
+public class EnemyDef
+{
+	// This class enables us to define various enemies
+	public string str;
+	public GameObject go;
+}
+
 public class LayoutTiles : MonoBehaviour
 {
 	static public LayoutTiles S;
@@ -16,12 +26,18 @@ public class LayoutTiles : MonoBehaviour
 									// ^ roomNumber as string allows encoding in the XML & rooms 0-F
 	public GameObject tilePrefab; // Prefab for all Tiles
 	public TileTex[] tileTextures; // A list of named textures for Tiles
+	public GameObject portalPrefab; // Prefab for the portals between rooms 
+	public EnemyDef[] enemyDefinitions; // Prefabs for Enemies
+
 	public bool ________________;
+
+	private bool firstRoom = true; // Is this the first room built?
 	public PT_XMLReader roomsXMLR;
 	public PT_XMLHashList roomsXML;
 	public Tile[,] tiles;
 	public Transform tileAnchor;
-	void Awake()
+
+	void Start()
 	{
 		S = this; // Set the Singleton for LayoutTiles
 				  // Make a new GameObject to be the TileAnchor (the parent transform of
@@ -52,6 +68,19 @@ public class LayoutTiles : MonoBehaviour
 	// Build a room from an XML <room> entry
 	public void BuildRoom(PT_XMLHashtable room)
 	{
+		// Destroy any old Tiles
+		foreach (Transform t in tileAnchor)
+		{ // Clear out old tiles
+		  // ^ You can iterate over a Transform to get its children
+			Destroy(t.gameObject);
+		}
+		// Move the Mage out of the way
+		Mage.S.pos = Vector3.left * 1000;
+		// ^ This keeps the Mage from accidentally triggering OnTriggerExit() on
+		// a Portal. In my testing, I found that OnTriggerExit was being called
+		// at strange times.
+		Mage.S.ClearInput(); // Cancel any active mouse input and drags
+		string rNumStr = room.att("num");
 		// Get the texture names for the floors and walls from <room> attributes
 		string floorTexStr = room.att("floor");
 		string wallTexStr = room.att("wall");
@@ -72,6 +101,7 @@ public class LayoutTiles : MonoBehaviour
 		GameObject go;
 		int height;
 		float maxY = roomRows.Length - 1;
+		List<Portal> portals = new List<Portal>();
 		// These loops scan through each tile of each row of the room
 		for (int y = 0; y < roomRows.Length; y++)
 		{
@@ -86,10 +116,9 @@ public class LayoutTiles : MonoBehaviour
 				{
 					case " ": // empty space
 					case "_": // empty space
-							  // Just skip over empty space
 						continue;
 					case ".": // default floor
-							  // Keep type="."
+						  // Keep type="."
 						break;
 					case "|": // default wall
 						height = 1;
@@ -120,10 +149,116 @@ public class LayoutTiles : MonoBehaviour
 				ti.type = type;
 				ti.height = height;
 				ti.tex = tileTexStr;
+				// If the type is still rawType, continue to the next iteration
+				if (rawType == type) continue;
+				// Check for specific entities in the room
+				switch (rawType)
+				{ // 1
+					case "X": // Starting position for the Mage
+							  // Mage.S.pos = ti.pos; // Uses the Mage Singleton
+						if (firstRoom)
+						{
+							Mage.S.pos = ti.pos; // Uses the Mage Singleton
+							roomNumber = rNumStr;
+							// ^ Setting roomNumber now keeps any portals from
+							// moving the Mage to them in this first room.
+							firstRoom = false;
+						}
+						break;
+					case "0": // Numbers are room portals (up to F in hexadecimal)
+					case "1": // This allows portals to be placed in the Rooms.xml file
+					case "2":
+					case "3":
+					case "4":
+					case "5":
+					case "6":
+					case "7":
+					case "8":
+					case "9":
+					case "A":
+					case "B":
+					case "C":
+					case "D":
+					case "E":
+					case "F":
+						// Instantiate a Portal
+						GameObject pGO = Instantiate(portalPrefab) as GameObject;
+						Portal p = pGO.GetComponent<Portal>();
+						p.pos = ti.pos;
+						p.transform.parent = tileAnchor;
+						// ^ Attaching this to the tileAnchor means that the Portal
+						// will be Destroyed when a new room is built
+						p.toRoom = rawType;
+						portals.Add(p);
+						break;
+					default:
+						// Try to see if there's an Enemy for that letter
+						Enemy en = EnemyFactory(rawType);
+						if (en == null) break; // If there's not one, break out
+											   // Set up the new Enemy
+						en.pos = ti.pos;
+						// Make en a child of tileAnchor so it's deleted when the
+						// next room is loaded.
+						en.transform.parent = tileAnchor;
+						en.typeString = rawType;
+						break;
+				}
 				// More to come here...
 			}
 		}
+		// Position the Mage
+		foreach (Portal p in portals)
+		{
+			// If p.toRoom is the same as the room number the Mage just exited,
+			// then the Mage should enter this room through this Portal
+			// Alternatively, if firstRoom == true and there was no X in the
+			// room (as a default Mage starting point), move the Mage to this
+			// Portal as a backup measure (if, for instance, you want to just
+			// load room number "5")
+			if (p.toRoom == roomNumber || firstRoom)
+			{
+				// ^ If there's an X in the room, firstRoom will be set to false
+				// by the time the code gets here
+				Mage.S.StopWalking(); // Stop any Mage movement
+				Mage.S.pos = p.pos; // Move _Mage to this Portal location
+									// _Mage maintains her facing from the previous room, so there
+									// is no need to rotate her in order for her to enter this room
+									// facing the right direction.
+				p.justArrived = true;
+				// ^ Tell the Portal that Mage has just arrived.
+				firstRoom = false;
+				// ^ Stops a 2nd Portal in this room from moving the Mage to it
+			}
+		}
+		// Finally assign the roomNumber
+		roomNumber = rNumStr;
+		
 	}
+
+	public Enemy EnemyFactory(string sType)
+	{
+		// See if there's an EnemyDef with that sType
+		GameObject prefab = null;
+		foreach (EnemyDef ed in enemyDefinitions)
+		{
+			if (ed.str == sType)
+			{
+				prefab = ed.go;
+				break;
+			}
+		}
+		if (prefab == null)
+		{
+			Utils.tr("LayoutTiles.EnemyFactory()", "No EnemyDef for: " + sType);
+			return (null);
+		}
+		GameObject go = Instantiate(prefab) as GameObject;
+		// The generic form of GetComponent (with the <>) won't work for
+		// interfaces like Enemy, so we must use this form instead.
+		Enemy en = (Enemy)go.GetComponent(typeof(Enemy));
+		return (en);
+	}
+
 	// Build a room based on room number. This is an alternative version of
 	// BuildRoom that grabs roomXML based on num.
 	public void BuildRoom(string rNumStr)
